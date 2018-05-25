aab3e712102bf6e43df35b2d886db6f5b1882670
mateussangalli <37818285+mateussangalli@users.noreply.github.com>
2018-05-24 21:38:59 -0300

    Add files via upload

--- mateus/lstm-cnn/lstm-cnn.py
+++ mateus/lstm-cnn/lstm-cnn.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
 
-import re
-
-import sys
 import os
 
 os.environ['KERAS_BACKEND']='tensorflow'
@@ -13,24 +10,29 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.callbacks import TensorBoard
 from keras.utils import to_categorical
 
-from keras.layers import Dense, Input, Flatten, Embedding, Conv1D, MaxPooling1D
+from keras.layers import Dense, Input, Flatten, Embedding, Conv1D, MaxPooling1D, GlobalMaxPooling1D
 from keras.layers import Bidirectional, LSTM
 from keras.layers import GaussianNoise, Dropout
 from keras.regularizers import l2
-from keras.optimizers import Adam
+from keras.optimizers import Adamax
 from keras.models import Model
 
 import pickle
 
-MAX_SEQUENCE_LENGTH = 150
+MAX_SEQUENCE_LENGTH = 200
 MAX_WORDS = 30000
-EMBEDDING_DIM = 100
+EMBEDDING_DIM = 50
 
 tb_folder = 'lstm_keras_tb'
 emb_meta = 'emb_labels.tsv'
 filename_tokenizer = 'tokenizer.pickle'
 filename_model = 'model_lstm-cnn.json'
 filename_weights = 'model_lstm-cnn.h5'
+filename_corpus = '../datasets/corpus.txt'
+
+# this is the number of times we will train throught the whole training set
+# the epoch value will be more than that because the dataset is divided
+num_epochs = 2
 
 tb_callback = TensorBoard(
         tb_folder,
@@ -38,53 +40,26 @@ tb_callback = TensorBoard(
         embeddings_layer_names=['embeddings'],
         embeddings_metadata={'embeddings':emb_meta})
 
-train_data = pd.read_json('../datasets/train_Books.json', lines=True)
-
-texts = [text for text in train_data['reviewText']]
-labels = [label for label in train_data['overall']]
-del train_data
-
-# create a new Tokenizer
-tokenizer = Tokenizer(num_words=MAX_WORDS, filters=';#&*:()')
-tokenizer.fit_on_texts(texts)
-sequences = tokenizer.texts_to_sequences(texts)
-del texts
-sizes = np.array([len(sequence) for sequence in sequences])
-# saving Tokenizer
-with open(filename_tokenizer, 'wb') as f:
-    pickle.dump(tokenizer, f, protocol=pickle.HIGHEST_PROTOCOL)
-
+# just have to compute the tokenizer once
+try:
+    # loading
+    with open(filename_tokenizer, 'rb') as handle:
+        tokenizer = pickle.load(handle)
+except:
+    # create a new Tokenizer
+    tokenizer = Tokenizer(num_words=MAX_WORDS, filters=';#&*:()')
+    with open(filename_corpus) as corpus:
+        tokenizer.fit_on_texts(corpus)
+    # saving Tokenizer
+    with open(filename_tokenizer, 'wb') as f:
+        pickle.dump(tokenizer, f, protocol=pickle.HIGHEST_PROTOCOL)
 word_index = tokenizer.word_index
 print('Found %s unique tokens.' % len(word_index))
 
-
-x_train = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-del sequences
-y_train = np.asarray(labels)-1
-del labels
-print('Shape of data tensor:', x_train.shape)
-print('Shape of label tensor:', y_train.shape)
-indices = np.arange(x_train.shape[0])
-np.random.shuffle(indices)
-x_train = x_train[indices]
-y_train = to_categorical(y_train[indices])
-
-# process validation set
-val_data = pd.read_json('../datasets/val_Books.json', lines=True)
-texts = [text for text in val_data['reviewText']]
-labels = [label for label in val_data['overall']]
-del val_data
-sequences = tokenizer.texts_to_sequences(texts)
-del texts
-x_val = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-y_val = to_categorical(np.asarray(labels)-1)
-del sequences
-del labels
-
 embedding_matrix = np.random.random((MAX_WORDS, EMBEDDING_DIM))
-
+# use the pre-trained glove vectors
 embeddings_index = {}
-glove_filename = '../vectors.txt'
+glove_filename = '../glove.6B/glove.6B.50d.txt'
 mean = np.array([0]*EMBEDDING_DIM, dtype='float32')
 with open(glove_filename) as f:
     for line in f:
@@ -94,15 +69,14 @@ with open(glove_filename) as f:
         mean += coefs
         embeddings_index[word] = coefs
 mean = mean/len(embeddings_index)
-
 inv_index = {v: k for k, v in word_index.items()}
 for i in range(1,MAX_WORDS):
     word = inv_index[i]
     embedding_vector = embeddings_index.get(word)
     if embedding_vector is not None:
-        # words not found in embedding index will be all-zeros.
         embedding_matrix[i] = embedding_vector
     else:
+        # words not found in embedding index will be close to the mean.
         embedding_matrix[i] = mean + np.random.normal(scale=0.1, size=(1,EMBEDDING_DIM))
 del embeddings_index
 
@@ -122,32 +96,67 @@ with open(tb_folder+'/'+emb_meta,'w') as emb:
 
 sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
 embedded_sequences = embedding_layer(sequence_input)
-l_noise = GaussianNoise(0.01)(embedded_sequences)
-l_dropout = Dropout(0.05)(l_noise)
+l_noise = GaussianNoise(0.05)(embedded_sequences)
 
-l_lstm = Bidirectional(LSTM(64, return_sequences=True, recurrent_activation='hard_sigmoid', kernel_regularizer=l2(0.0015)))(l_dropout)
+l_lstm = Bidirectional(LSTM(30, return_sequences=True, recurrent_activation='hard_sigmoid', kernel_regularizer=l2(0.0015), dropout=0.3))(l_noise)
 
 l_cov1 = Conv1D(150, 5, activation='relu', padding='same', kernel_regularizer=l2(0.0015))(l_lstm)
-l_pool1 = MaxPooling1D(strides=2)(l_cov1)  # global max pooling
+l_pool1 = MaxPooling1D(strides=4)(l_cov1)  # global max pooling
 
 l_cov2 = Conv1D(200, 5, activation='relu', padding='same', kernel_regularizer=l2(0.0015))(l_pool1)
-l_pool2 = MaxPooling1D(strides=2)(l_cov2)  # global max pooling
+l_pool2 = MaxPooling1D(strides=8)(l_cov2)  # global max pooling
 
-l_cov3 = Conv1D(250, 5, activation='relu', padding='same', kernel_regularizer=l2(0.0015))(l_pool2)
-l_pool3 = MaxPooling1D(strides=3)(l_cov3)  # global max pooling
+l_cov3 = Conv1D(200, 3, activation='relu', padding='same', kernel_regularizer=l2(0.0015))(l_pool2)
+l_pool3 = GlobalMaxPooling1D()(l_cov3)  # global max pooling
 
-l_cov4 = Conv1D(300, 3, activation='relu', padding='same', kernel_regularizer=l2(0.0015))(l_pool3)
-l_pool4 = MaxPooling1D(strides=4)(l_cov4)  # global max pooling
+l_dense = Dense(150, activation='relu', kernel_regularizer=l2(0.0015))(l_pool3)
+l_dropout = Dropout(0.5)(l_dense)
 
-l_flat = Flatten()(l_pool4)
-preds = Dense(5, activation='softmax', kernel_regularizer=l2(0.0015))(l_flat)
+preds = Dense(5, activation='softmax')(l_dropout)
 
 model = Model(sequence_input, preds)
-model.compile(loss='categorical_crossentropy',optimizer=Adam(clipnorm=4.),metrics=['acc'])
+model.compile(loss='categorical_crossentropy',optimizer=Adamax(clipnorm=4.),metrics=['acc'])
 
 model.summary()
-model.fit(x_train, y_train, validation_data=(x_val, y_val),epochs=3,
-        batch_size=32, callbacks=[tb_callback])
+
+# epoch will not be the real number of the epoch, because the dataset is divided
+epoch = 0
+for i in range(num_epochs):
+    train_data = pd.read_json('../datasets/train_Books.json', lines=True, chunksize=500000)
+    for chunk in train_data:
+        texts = chunk['reviewText'].tolist()
+        labels = chunk['overall'].tolist()
+        sequences = tokenizer.texts_to_sequences(texts)
+        del texts
+        x_train = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+        del sequences
+        y_train = to_categorical(np.asarray(labels)-1)
+        del labels
+        model.fit(x_train, y_train,
+                initial_epoch=epoch, class_weight='auto',
+                epochs=epoch+1, batch_size=32,
+                callbacks=[tb_callback], shuffle=True)
+        epoch += 1
+        del x_train
+        del y_train
+        # evaluate on the validation set each 5 chunks
+        if epoch%5 == 0:
+            val_data = pd.read_json('../datasets/val_Books.json', lines=True, chunksize=200000)
+            for vchunk in val_data:
+                texts = vchunk['reviewText'].tolist()
+                labels = vchunk['overall'].tolist()
+                sequences = tokenizer.texts_to_sequences(texts)
+                del texts
+                x_val = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+                del sequences
+                y_val = to_categorical(np.asarray(labels)-1)
+                del labels
+                loss, acc = model.evaluate(x_val, y_val, verbose=1)
+                print('loss = ' + str(loss))
+                print('acc = ' + str(acc))
+                del x_val
+                del y_val
+
 
 
 # serialize model to JSON
@@ -158,23 +167,34 @@ with open(filename_model, 'w') as json_file:
 model.save_weights(filename_weights)
 print('Saved model to disk')
 
-del x_train
-del y_train
-del x_val
-del y_val
-
-test_data = pd.read_json('../datasets/test_Books.json', lines=True)
-test_data = test_data.sample(frac = 0.2)
-
-texts = [text for text in test_data['reviewText']]
-labels = [label for label in test_data['overall']]
-sequences = tokenizer.texts_to_sequences(texts)
-x_test = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-y_test = to_categorical(np.asarray(labels)-1)
-del sequences
-del labels
-del texts
-
-loss, acc = model.evaluate(x_test, y_test, verbose=1)
-print(loss)
-print(acc)
+# evaluation
+val_data = pd.read_json('../datasets/val_Books.json', lines=True, chunksize=200000)
+for chunk in val_data:
+    texts = chunk['reviewText'][chunk['overall']==i].tolist()
+    labels = chunk['overall'][chunk['overall']==i].tolist()
+    sequences = tokenizer.texts_to_sequences(texts)
+    del texts
+    x_val = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+    del sequences
+    y_val = to_categorical(np.asarray(labels)-1)
+    del labels
+    loss, acc = model.evaluate(x_val, y_val, verbose=1)
+    print('loss = ' + str(loss))
+    print('acc = ' + str(acc))
+    del x_val
+    del y_val
+    #evaluate the accuracy for each individual class individually
+    for i in range(1,6):
+        texts = chunk['reviewText'][chunk['overall']==i].tolist()
+        labels = chunk['overall'][chunk['overall']==i].tolist()
+        sequences = tokenizer.texts_to_sequences(texts)
+        del texts
+        x_val = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+        del sequences
+        y_val = to_categorical(np.asarray(labels)-1)
+        del labels
+        loss, acc = model.evaluate(x_val, y_val, verbose=1)
+        print('loss_'+str(i)+' = ' + str(loss))
+        print('acc _'+str(i)+' = ' + str(acc))
+        del x_val
+        del y_val
--- mateus/lstm-cnn/test-model.py
+++ mateus/lstm-cnn/test-model.py
@@ -16,13 +16,13 @@ from keras.utils.np_utils import to_categorical
 from keras.layers import Embedding
 from keras.layers import Dense, Input, Flatten
 from keras.layers import Conv1D, MaxPooling1D, Embedding, Merge, Dropout
-from keras.optimizers import Adam
+from keras.optimizers import Adamax
 from keras.models import Model
 from keras.models import model_from_json
 
 import pickle
 
-MAX_SEQUENCE_LENGTH = 256
+MAX_SEQUENCE_LENGTH = 250
 
 filename_tokenizer = 'tokenizer.pickle'
 filename_model = 'model_lstm-cnn.json'
@@ -38,36 +38,36 @@ model = model_from_json(loaded_model_json)
 
 model.load_weights(filename_weights)
 model.summary()
+model.compile(loss='categorical_crossentropy', optimizer=Adamax(clipnorm=4.), metrics=['acc'])
 
+test_data = pd.read_json('../datasets/test_Books_big.json', lines=True, chunksize=100000)
 
-test_data = pd.read_json('../datasets/test_Books.json', lines=True)
-test_data = test_data.sample(frac = 0.1)
-
-texts = [text for text in test_data['reviewText']]
-labels = [label for label in test_data['overall']]
-sequences = tokenizer.texts_to_sequences(texts)
-x_test = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-y_test = np.asarray(labels)-1
-del sequences
-del labels
-del texts
-
-pred_test = model.predict(x_test, verbose=1)
-pred_test = pred_test.reshape(1,pred_test.size)
-print(pred_test[:10])
-print(y_test[:10])
-print(pred_test[:-10])
-print(y_test[:-10])
-
-
-diff = pred_test - y_test
-diff = np.absolute(diff)
-print(np.sum(diff)/diff.size)
-
-num_rights = 1*(diff < 0.5) 
-acc = np.sum(num_rights)/num_rights.size
-print(acc)
-
-num_rights = 1*(diff < 1) 
-acc = np.sum(num_rights)/num_rights.size
-print(acc)
+test_data = pd.read_json('../datasets/test_Books.json', lines=True, chunksize=100000)
+for chunk in test_data:
+    texts = chunk['reviewText'][chunk['overall']==i].tolist()
+    labels = chunk['overall'][chunk['overall']==i].tolist()
+    sequences = tokenizer.texts_to_sequences(texts)
+    del texts
+    x_test = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+    del sequences
+    y_test = to_categorical(np.asarray(labels)-1)
+    del labels
+    loss, acc = model.evaluate(x_test, y_test, verbose=1)
+    print('loss = ' + str(loss))
+    print('acc = ' + str(acc))
+    del x_test
+    del y_test
+    for i in range(1,6):
+        texts = chunk['reviewText'][chunk['overall']==i].tolist()
+        labels = chunk['overall'][chunk['overall']==i].tolist()
+        sequences = tokenizer.texts_to_sequences(texts)
+        del texts
+        x_test = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+        del sequences
+        y_test = to_categorical(np.asarray(labels)-1)
+        del labels
+        loss, acc = model.evaluate(x_test, y_test, verbose=1)
+        print('loss_'+str(i)+' = ' + str(loss))
+        print('acc _'+str(i)+' = ' + str(acc))
+        del x_test
+        del y_test
