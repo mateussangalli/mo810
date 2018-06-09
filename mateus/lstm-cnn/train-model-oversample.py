526a25247ea6fdc975ca687dc5abcf3c497c6038
mateussangalli <37818285+mateussangalli@users.noreply.github.com>
2018-06-08 22:38:44 -0300

    fixed an index error

--- mateus/lstm-cnn/train-model-oversample.py
+++ mateus/lstm-cnn/train-model-oversample.py
@@ -17,18 +17,18 @@ from keras.models import Model
 
 import pickle
 
-MAX_SEQUENCE_LENGTH = 250
-MAX_WORDS = 30000
-EMBEDDING_DIM = 50
+MAX_SEQUENCE_LENGTH = 300
+MAX_WORDS = 50000
+EMBEDDING_DIM = 100
 
 tb_folder = 'lstm_keras_tb'
 emb_meta = 'emb_labels.tsv'
 filename_tokenizer = 'tokenizer.pickle'
-filename_model = 'model_lstm-cnn.h5'
+filename_model = 'model_cnn-lstm.h5'
 
 # this is the number of times we will train throught the whole training set
 # the epoch value will be more than that because the dataset is divided
-num_epochs = 29
+num_epochs = 24
 
 folder = '../datasets'
 
@@ -78,38 +78,57 @@ model = load_model(filename_model)
 
 model.summary()
 
+val_data = pd.read_json('../datasets/val_Books.json', lines=True).sample(50000)
+texts = val_data['reviewText'].tolist()
+labels = val_data['overall'].tolist()
+sequences = tokenizer.texts_to_sequences(texts)
+x_val = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH, truncating='post')
+y_val = to_categorical(np.asarray(labels)-1)
+
+del val_data
+del texts
+del labels
+del sequences
+
+loss_train = np.array([0]*(num_epochs+1), dtype=np.float32)
+acc_train = np.array([0]*(num_epochs+1), dtype=np.float32)
+
+loss_val = list()
+acc_val = list()
+
 for epoch, x_train, y_train in input_gen(folder,train_filenames,train_numlines,tokenizer,100000,num_epochs):
     model.fit(x_train, y_train,initial_epoch=epoch,
             epochs=epoch+1, batch_size=32,
             callbacks=[tb_callback], shuffle=True)
     if (epoch+1) % 5 == 0:
-        # save model
+        loss, acc = model.evaluate(x_train, y_train, verbose=1)
+        print('train loss = %.3f' % loss_train[epoch])
+        print('train acc = %.3f' % acc_train[epoch])
+        loss_train.append(loss)
+        acc_train.append(acc)
+        loss, acc = model.evaluate(x_val, y_val, verbose=1)
+        print('val loss = %.3f' % loss)
+        print('val acc = %.3f' % acc)
+        loss_val.append(loss)
+        acc_val.append(acc)
+        # save checkpoint
         model.save('ckpt_%s.h5' % (epoch+1))
-        print('Saved model to disk')
+        print('Saved checkpoint to disk')
+
+loss_train = np.array(loss_train)
+acc_train = np.array(acc_train)
+loss_val = np.array(loss_val)
+acc_val = np.array(acc_val)
 
 # save model
 model.save(filename_model)
 print('Saved model to disk')
 
-# evaluation
-val_data = pd.read_json('../datasets/val_Books.json', lines=True, chunksize=200000)
-for chunk in val_data:
-    texts = chunk['reviewText'].tolist()
-    labels = chunk['overall'].tolist()
-    sequences = tokenizer.texts_to_sequences(texts)
-    x_val = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH, truncating='post')
-    y_val = to_categorical(np.asarray(labels)-1)
-    loss, acc = model.evaluate(x_val, y_val, verbose=1)
-    print('loss = ' + str(loss))
-    print('acc = ' + str(acc))
-    del x_val
-    del y_val
-    #evaluate the accuracy for each individual class individually
-    for i in range(1,6):
-        x_val = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)[chunk['overall']==i]
-        y_val = to_categorical(np.asarray(labels)-1)[chunk['overall']==i]
-        loss, acc = model.evaluate(x_val, y_val, verbose=1)
-        print('loss_'+str(i)+' = ' + str(loss))
-        print('acc _'+str(i)+' = ' + str(acc))
-        del x_val
-        del y_val
+d = dict()
+d['loss_train'] = loss_train
+d['loss_val'] = loss_val
+d['acc_train'] = acc_train
+d['acc_val'] = acc_val
+# saving losses and accuracys
+with open('losses.pickle', 'wb') as f:
+    pickle.dump(d, f, protocol=pickle.HIGHEST_PROTOCOL)
